8e5eb779746650f8490e3aaf7fd076953d5cb6b3
Patrick Munnich <munnich@protonmail.com>
2026-01-25 18:20:41 +0100

    add chunking support

--- src/cxb_parser.hpp
+++ src/cxb_parser.hpp
@@ -4,12 +4,13 @@
 #include <vector>
 #include <stdint.h>
 #include <pugixml.hpp>
+#include <array>
 
 struct CXBFile {
     std::string name;
     std::string rawXml;
 };
-
+constexpr std::array<uint8_t, 2> CXB_CHUNK_MARKER = {0x01, 0x0A};
 
 std::map<std::string, std::string> ExtractCXB(const std::string& path);
-std::vector<uint8_t> ConvertToCXB(const std::vector<CXBFile>& files);
+std::vector<uint8_t> ConvertToCXB(const std::vector<CXBFile>& files, uint8_t nameFieldSize);
